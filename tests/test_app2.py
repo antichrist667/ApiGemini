0a895f2047487077bcabbb1581dd2c646879fcbe
Antichrist667 <cristian.caiza0310@gmail.com>
2025-06-19 17:13:01 -0500

    sin test/

--- tests/test_app2.py
+++
@@ -1,7 +0,0 @@
-
-from app2 import invertir_texto
-
-def test_invertir_texto():
-    assert invertir_texto("Hola") == "aloH"
-    assert invertir_texto("12345") == "54321"
-    assert invertir_texto("") == ""
